e4d4a280e51f062079cba26f6fe03d1bb86e5c5e
afshankhan786 <profit.afshankhan@gmail.com>
2026-03-29 12:01:20 +0530

    update

--- src/main/java/com/spring/mvc/controller/EmployeeController.java
+++ src/main/java/com/spring/mvc/controller/EmployeeController.java
@@ -22,21 +22,6 @@ public class EmployeeController {
 	EmployeeService employeeService;
 	
 	
-	static Map<Integer,Employee> map=null;
-	static 	int empId=0;
-
-	static {
-		 map = new HashMap<>();
-		 map.put(++empId, new Employee (empId,"Mukash", "IT","Noida"));
-		 map.put(++empId, new Employee (empId,"Shurash", "IOT","Noida"));
-		 map.put(++empId, new Employee (empId,"Mahash", "AI","Noida"));
-		 map.put(++empId, new Employee (empId,"Nirash", "JEE","Noida"));
-		 map.put(++empId, new Employee (empId,"Nitesh", "BBC","Noida"));
-
-		 
-	}
-	
-	
 	
 	@GetMapping("/")
 	public String Welcome() {
@@ -52,20 +37,7 @@ public class EmployeeController {
 	
 	
 
-	@PostMapping("/singupAdmin")
-	public String postAdmin(@RequestParam(name = "adminname")String adminname,@RequestParam(name = "department")String department,@RequestParam(name = "address")String adderss,Model model) {
-		
-		System.out.println("adminname :"+ adminname);
-		System.out.println("department :"+department);
-		System.out.println("adderss :" +adderss);
-		
-		model.addAttribute("adminname", adminname);
-		model.addAttribute("department",department);
-		model.addAttribute("adderss", adderss);
-		
-		return "success";
-		
-	}
+	
 	
 	@PostMapping("/creatAdmin")
 	public String creatAdmin(@ModelAttribute Employee emp,Model model) {
@@ -77,11 +49,15 @@ public class EmployeeController {
 	
 	@GetMapping("/getEmpById/{id}")
 	public String getEmp(@PathVariable(name = "id") int id,Model model) {
-		Employee e = map.get(id);
-		model.addAttribute("employee",e);
 		return "success"; 
 		
 	}
 	
+	@GetMapping("/employees")
+	public String listEmployees(Model model) {
+		model.addAttribute("employees",employeeService.getAllEmployees());
+		return "employeeList";
+		
+	}
 	
 }
--- src/main/java/com/spring/mvc/service/EmployeeService.java
+++ src/main/java/com/spring/mvc/service/EmployeeService.java
@@ -1,5 +1,7 @@
 package com.spring.mvc.service;
 
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,10 @@ public class EmployeeService {
 	public void saveEmployee(Employee emp) {
 		System.out.println("EmployeeService.saveEmployee()");
 		employeeDao.saveEmployee(emp);
+	}
+
+	public List<Employee> getAllEmployees() {
+		return employeeDao.getAllEmployees();
 	}
 	
 	
